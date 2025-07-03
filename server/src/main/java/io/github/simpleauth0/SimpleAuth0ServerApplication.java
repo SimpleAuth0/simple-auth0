9bc94e95026823e553579b4885327bf147c9b9f2
ReLive27 <re_live27@163.com>
2025-07-03 22:23:20 +0800

    Add dependencies

--- server/src/main/java/io/github/simpleauth0/SimpleAuth0ServerApplication.java
+++ server/src/main/java/io/github/simpleauth0/SimpleAuth0ServerApplication.java
@@ -1,5 +1,6 @@
-package io.github.simpleauth0.server;
+package io.github.simpleauth0;
 
+import io.github.simpleauth0.security.configuration.EnableAuthentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: ReLive27
  * @date: 2025/3/5 23:08
  */
+@EnableAuthentication
 @SpringBootApplication
 public class SimpleAuth0ServerApplication {
 
--- server/src/main/java/io/github/simpleauth0/package-info.java
+++ server/src/main/java/io/github/simpleauth0/package-info.java
@@ -1,4 +1,4 @@
 /**
  * 提供 simpleauth0 系统的服务启动与对外接口入口。
  */
-package io.github.simpleauth0.server;
+package io.github.simpleauth0;
