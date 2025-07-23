fee9463e83903029267d133d00bcda6178058ce9
ReLive27 <re_live27@163.com>
2025-07-23 22:31:21 +0800

    fix make.sh

--- docs/make.sh
+++ docs/make.sh
@@ -4,7 +4,7 @@ echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
 npx @redocly/cli build-docs ./docs/openapi/simple-auth0.yaml && \
-mv ./docs/redoc-static.html ./docs/index.html && \
+mv ./redoc-static.html ./docs/index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
 #sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
