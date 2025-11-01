f8933680e6d8fbec292fa29544a5818d622addb3
ReLive27 <re_live27@163.com>
2025-11-01 22:08:00 +0800

    fix(config): ensure SimpleAuth0Properties is loaded before OpaqueTokenConfiguration

--- core/src/main/java/io/github/simpleauth0/core/configuration/AuthTokenAutoConfiguration.java
+++ core/src/main/java/io/github/simpleauth0/core/configuration/AuthTokenAutoConfiguration.java
@@ -1,17 +1,14 @@
 package io.github.simpleauth0.core.configuration;
 
-import io.github.simpleauth0.core.SimpleAuth0Properties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 /**
  * @author: ReLive27
  * @date: 2025/6/30 22:42
  */
-@EnableConfigurationProperties(SimpleAuth0Properties.class)
 @AutoConfiguration(before = SecurityAutoConfiguration.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Import({AuthTokenConfiguration.JwtConfigurer.class,
